3d48f8fbddf4186bf6d67f47b441d1d0bf348716
WildCard <neeleshverma124@gmail.com>
2025-04-16 15:35:07 +0530

    Add recommendations, createProduct routes

--- backend/routes/product.route.js
+++ backend/routes/product.route.js
@@ -2,6 +2,11 @@ import { Router } from "express";
 import {
   getAllProducts,
   getFeaturedProducts,
+  createProduct,
+  deleteProduct,
+  getRecommendedProducts,
+  getProductsByCategory,
+  toggleFeaturedProduct,
 } from "../controllers/product.controller.js";
 import { protectRoute, adminRoute } from "../middleware/auth.middleware.js";
 
@@ -9,6 +14,10 @@ const router = Router();
 
 router.get("/", protectRoute, adminRoute, getAllProducts);
 router.get("/featured", getFeaturedProducts);
+router.get("/category/:category", getProductsByCategory);
+router.get("/recommendations", getRecommendedProducts);
 router.post("/", protectRoute, adminRoute, createProduct);
+router.patch("/:id", protectRoute, adminRoute, toggleFeaturedProduct);
+router.delete("/:id", protectRoute, adminRoute, deleteProduct);
 
 export default router;
